0a78060e987e8cc44165c50d7f471624dbdfc3bf
ivan <ivan2adjiri@gmail.com>
2023-08-24 14:56:23 +0000

    Simple Calculator Project

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+var num1 = prompt("Enter your first number:");
+
+var num2 = prompt("Enter your second number:");
+
+num1 = parseInt(num1,10);
+
+num2 = parseInt(num2,10);
+
+var sum = num1 + num2;
+
+var sub = num1 - num2;
+
+var mul = num1 * num2;
+
+var div = num1 / num2;
+
+
+
+
+
+document.write("Addition:" +sum +" </br>");
+
+document.write("Subtraction:" +sub + "</br>");
+
+document.write("Multiplication:" +mul + "</br>");
+
+document.write("Division:" +div + "</br>");
